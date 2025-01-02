6e7a39ebdf541a7d6ed91acd4834772e2d952f76
Mila <mila.gastelum@autistici.org>
2025-01-02 22:49:43 +0000

    omni_amogus_doha.mk: change device name

--- omni_amogus_doha.mk
+++ omni_amogus_doha.mk
@@ -18,12 +18,7 @@ $(call inherit-product, device/motorola/amogus_doha/device.mk)
 PRODUCT_DEVICE := amogus_doha
 PRODUCT_NAME := omni_amogus_doha
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto g(8) family
+PRODUCT_MODEL := moto g(8) plus
 PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_GMS_CLIENTID_BASE := android-google
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="lineage_amogus_doha-userdebug 13 TQ3A.230901.001 eng.wangtk.20241227.153708 test-keys"
-
-BUILD_FINGERPRINT := motorola/lineage_amogus_doha/amogus_doha:13/TQ3A.230901.001/wangtkl01021148:userdebug/test-keys
